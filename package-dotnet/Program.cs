ae2252d8479391aa3889fd06932fdd87634e8cfe
Shahan Ahmed <shahanahmeddev@gmail.com>
2024-05-13 22:35:40 +0000

    create an azure resource group using the C# SDK

---
+++ package-dotnet/Program.cs
@@ -0,0 +1,31 @@
+﻿using Azure;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Resources.Models;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string subscriptionId = "c19d82c6-c1f6-4f4a-bcd8-b3d96af29337";
+        string resourceGroupName = "helloworld1dmehnatdunya";
+        string location = "westus"; // e.g., "eastus"
+
+        // Authenticate and create the ARM client
+        ArmClient armClient = new ArmClient(new DefaultAzureCredential());
+
+        // Get the subscription
+        SubscriptionResource subscription = await armClient.GetDefaultSubscriptionAsync();
+
+        // Create or update the resource group
+        ResourceGroupCollection resourceGroups = subscription.GetResourceGroups();
+        ResourceGroupData resourceGroupData = new ResourceGroupData(location);
+        ArmOperation<ResourceGroupResource> resourceGroupOperation = await resourceGroups.CreateOrUpdateAsync(WaitUntil.Completed, resourceGroupName, resourceGroupData);
+        ResourceGroupResource resourceGroup = resourceGroupOperation.Value;
+
+        Console.WriteLine($"Resource Group '{resourceGroupName}' created successfully in location '{location}'.");
+    }
+}
